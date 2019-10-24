05deedf7ad6e35c02cd097bea4808fcd4af900ec
mdfazal <fazalmd964@gmail.com>
2019-10-24 15:17:00 +0530

    salary program added

---
+++ currency.c
@@ -0,0 +1,5 @@
+/*Consider a currency system with notes of seven denomi nations, namely, Re. 1,
+Rs. 2, Rs. 5, Rs. 10, Rs. 50 and Rs. 100. If Rs. N has to be disbursed
+using this currency system. Write a program to print the number of notes for
+all denominations such that the total number of notes disbursed to pay Rs. N is
+minimum. (Note: Do it without conditional statements)*/
---
+++ salary.c
@@ -0,0 +1,16 @@
+/*Ramesh's basic salary is input through the keyboard. His dearness allowance is 40% of basic salary, and house rent allowance is 20%of basic salary.Write a program to calculate his gross salary*/
+
+#include <stdio.h>
+int main()
+{
+	double bs,da,rent;
+	double gs;
+	printf("Enter the basic salary\n");
+	scanf("%lf",&bs);
+	da = 0.4*bs;
+	rent = 0.2*bs;
+	gs=(bs+da+rent); /*gross salary = sum of basic & all allowances*/
+	printf("the gross salary is %f \n",gs);
+	//return 0;
+}
+
