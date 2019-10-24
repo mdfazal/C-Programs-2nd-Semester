fa224b87b2d6c8208b34093eb30c532b8e7482f4
mdfazal <fazalmd964@gmail.com>
2019-10-24 16:09:08 +0530

    maxmin program added

---
+++ maxmin.c
@@ -0,0 +1,7 @@
+Write a program to store three numbers into the variables a, b and c. Print the
+values of all the variables and find the variables with maximum and minimum
+numbers. Swap the values of variables with max and min values without using any
+other variable. Finally, print the value of all the variables to ensure that the swap
+has taken place.
+
+
