426b3eb4347e891863f3a5fd44a4931c266187f8
mdfazal <fazalmd964@gmail.com>
2019-10-24 15:57:10 +0530

    triangle question  program added

---
+++ triangle.c
@@ -0,0 +1,2 @@
+/*Write a program that reads three coordinates (x 1 , y 1 ), (x 2 , y 2 ) & (x 3 , y 3 ) as input and checks whether these numbers can be considered as the three vertices of a triangle. If the vertices can form a triangle then find the type (isosceles, equilateral or right-angled) of the triangle and the area of the triangle.*/
+
